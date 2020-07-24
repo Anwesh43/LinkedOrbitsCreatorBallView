8f36a355ff610155b659fdc117c289bc657de1b7
Anwesh43 <anweshthecool0@gmail.com>
2020-07-25 03:22:51 +0530

    adding fixes

--- orbitscreatorballview/src/main/java/com/anwesh/uiprojects/orbitscreatorballview/OrbitsCreatorBallView.kt
+++ orbitscreatorballview/src/main/java/com/anwesh/uiprojects/orbitscreatorballview/OrbitsCreatorBallView.kt
@@ -17,10 +17,10 @@ val colors : Array<String> = arrayOf("#3F51B5", "#4CAF50", "#2196F3", "#FF5722",
 val startSizeFactor : Float = 0.25f
 val endSizeFactor : Float = 0.4f
 val strokeFactor : Float = 90f
-val ballRFactor : Float = 0.1f
+val ballRFactor : Float = 0.03f
 val delay : Long = 20
 val circles : Int = 3
-val scGap : Float = 0.02f / circles
+val scGap : Float = 0.02f / (circles * 2)
 val backColor : Int = Color.parseColor("#BDBDBD")
 
 fun Int.inverse() : Float = 1f / this
@@ -32,13 +32,13 @@ fun Canvas.drawOrbitCreatorBall(scale : Float, w : Float, paint : Paint) {
     val sf : Float = scale.sinify()
     val start : Float = w * startSizeFactor
     val end : Float = w * endSizeFactor
-    val gap : Float = (start - end) / circles
+    val gap : Float = (end - start) / circles
     var si1 : Float = 0f
     var si2 : Float = 0f
     val ballR : Float = w * ballRFactor
     for (j in 0..(circles - 1)) {
         val r  : Float = start + j * gap
-        val sfi : Float = sf.divideScale(j, circles)
+        val sfi : Float = sf.divideScale(1, 2).divideScale(j, circles)
         if (sfi <= 0f) {
             break
         }
@@ -49,12 +49,12 @@ fun Canvas.drawOrbitCreatorBall(scale : Float, w : Float, paint : Paint) {
             si2 += sfi2
         }
         paint.style = Paint.Style.STROKE
-        drawArc(RectF(-r, -r, r, r), 0f, 360f * sfi1, false, paint)
+        drawArc(RectF(-r, -r, r, r), -90f, 360f * sfi1, false, paint)
     }
     paint.style = Paint.Style.FILL
     save()
     rotate(360f * si1)
-    drawCircle(0f, -(start + gap * si2), ballR, paint)
+    drawCircle(0f, -(start + gap * si2), ballR * sf.divideScale(0, 2), paint)
     restore()
 }
 
