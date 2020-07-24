11d0854febf4d2972d546d1a7e6a5e90e420f8a0
Anwesh43 <anweshthecool0@gmail.com>
2020-07-25 03:08:32 +0530

    adding view in activity

--- app/src/main/java/com/anwesh/uiprojects/linkedorbitscreatorballview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedorbitscreatorballview/MainActivity.kt
@@ -2,11 +2,19 @@ package com.anwesh.uiprojects.linkedorbitscreatorballview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.orbitscreatorballview.OrbitsCreatorBallView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        OrbitsCreatorBallView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
