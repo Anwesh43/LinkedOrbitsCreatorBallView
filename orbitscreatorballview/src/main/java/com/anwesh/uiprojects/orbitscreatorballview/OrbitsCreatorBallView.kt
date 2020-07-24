b14ce1cb077fbdc295f9406586f9a6f6c2e9f401
Anwesh43 <anweshthecool0@gmail.com>
2020-07-25 02:58:53 +0530

    adding OCBNode class

--- orbitscreatorballview/src/main/java/com/anwesh/uiprojects/orbitscreatorballview/OrbitsCreatorBallView.kt
+++ orbitscreatorballview/src/main/java/com/anwesh/uiprojects/orbitscreatorballview/OrbitsCreatorBallView.kt
@@ -131,4 +131,45 @@ class OrbitsCreatorBallView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class OCBNode(var i : Int, val state : State = State()) {
+
+        private var next : OCBNode? = null
+        private var prev : OCBNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < colors.size - 1) {
+                next = OCBNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawOCBNode(i, state.scale, paint)
+        }
+
+        fun update(cb : (Float) -> Unit) {
+            state.update(cb)
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : OCBNode {
+            var curr : OCBNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
